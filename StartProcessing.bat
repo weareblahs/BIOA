91d11b4f22e9a8a619053441e7df45a784fa2602
thisistyx-AUTO <tanyuxuan2005@gmail.com>
2022-01-01 23:32:30 +0800

    New test stuffs! (also, bug fixes!)

--- StartProcessing.bat
+++ StartProcessing.bat
@@ -5,9 +5,9 @@ for /f "delims=" %%i in ('LocalVariables config.ini image general_overlay_image'
 for /f "delims=" %%i in ('LocalVariables config.ini image quality') do set quality=%%i >nul
 for /f "delims=" %%i in ('LocalVariables config.ini image x') do set x=%%i >nul
 for /f "delims=" %%i in ('LocalVariables config.ini image y') do set y=%%i >nul
-for /f "delims=" %%i in ('LocalVariables config.ini imageoutformat') do set out=%%i >nul
+for /f "delims=" %%i in ('LocalVariables config.ini image outformat') do set out=%%i >nul
 for /f "delims=" %%i in ('LocalVariables config.ini ffmpeg_finetune_image_advanced custom_args') do set custom_args=%%i >nul
 
 :: Start process
 
-for %%i in (input\*.*) DO ffmpeg -i "%%i" -i "%ol%" -filter_complex "[0:v][1:v] overlay=%x%:%y%:enable='between(t,0,20)'" -q:v %quality% %custom_args% %cd%\output\%%~ni.%outformat%
+for %%i in (input\*.*) DO ffmpeg -i "%%i" -i "%ol%" -filter_complex "[0:v][1:v] overlay=%x%:%y%:enable='between(t,0,20)'" -q:v %quality% %custom_args% %cd%\output\%%~ni.%out%

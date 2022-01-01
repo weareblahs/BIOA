15ffca11bc7e02ead8c9cc4883c09d330ab71823
thisistyx-AUTO <tanyuxuan2005@gmail.com>
2022-01-01 22:44:00 +0800

    nightly?

---
+++ StartProcessing.bat
@@ -0,0 +1,13 @@
+@echo on
+:: Import variables - general
+for /f "delims=" %%i in ('LocalVariables config.ini image general_overlay_image') do set ol=%%i >nul
+:: Import variables - FFMPEG finetune
+for /f "delims=" %%i in ('LocalVariables config.ini image quality') do set quality=%%i >nul
+for /f "delims=" %%i in ('LocalVariables config.ini image x') do set x=%%i >nul
+for /f "delims=" %%i in ('LocalVariables config.ini image y') do set y=%%i >nul
+for /f "delims=" %%i in ('LocalVariables config.ini imageoutformat') do set out=%%i >nul
+for /f "delims=" %%i in ('LocalVariables config.ini ffmpeg_finetune_image_advanced custom_args') do set custom_args=%%i >nul
+
+:: Start process
+
+for %%i in (input\*.*) DO ffmpeg -i "%%i" -i "%ol%" -filter_complex "[0:v][1:v] overlay=%x%:%y%:enable='between(t,0,20)'" -q:v %quality% %custom_args% %cd%\output\%%~ni.%outformat%
